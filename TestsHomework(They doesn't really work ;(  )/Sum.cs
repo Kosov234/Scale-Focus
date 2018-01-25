1313108197287b94fe89144e78017288be9f4554
Kosov234 <32747671+Kosov234@users.noreply.github.com>
2018-01-26 01:26:23 +0200

    Tests for Sum method.

---
+++ TestsHomework(They doesn't really work ;(  )/Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public static class BasicMethods
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(3, 400000));
+        }
+        public static int Sum(int n,int m)
+        {
+            int s = 0;
+            for (int f = n; f <= m; f++)
+            {
+                s = s + f;
+            }
+            int result = s;
+            return result;
+        }
+
+    }
+}
---
+++ TestsHomework(They doesn't really work ;(  )/SumTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SumTests
+    {
+        [TestMethod]
+        public void SumTestWrongRange()
+        {
+            Assert.AreEqual(0,Methods.BasicMethods.Sum(10,6));
+        }
+        [TestMethod]
+        public void SumTestNegativeRange()
+        {
+            Assert.AreEqual(-40, Methods.BasicMethods.Sum(-10, -6));
+        }
+        [TestMethod]
+        public void SumTestNegativeWrongRange()
+        {
+            Assert.AreEqual(0, Methods.BasicMethods.Sum(-6,-10));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SumTestOutOfRange()
+        {
+            Methods.BasicMethods.Sum(2,2147483647);
+        }
+    }
+}
