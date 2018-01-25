97a86e9bfc82bf550fcf878ee506281c55cbdaec
Kosov234 <32747671+Kosov234@users.noreply.github.com>
2018-01-25 23:00:25 +0200

    TestsHomework
    At least i've tryed

---
+++ TestsHomework(They doesn't really work ;(  )/Average.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+   public static class BasicMethods
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(0)); ;
+        }
+        public static int Average(int n)
+        {
+            if (n==0)
+            {
+                throw new System.DivideByZeroException();
+                Console.WriteLine("NO :(");
+            }
+            int f = 0;
+            for (int s = 0; s <= n; s++)
+            {
+                f = f + s;
+            }
+            int result = f / n;
+            return result;
+        }
+    }
+    }
