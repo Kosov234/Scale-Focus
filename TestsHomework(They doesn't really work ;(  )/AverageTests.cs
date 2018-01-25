7ad1edaf25a6dcec2a576acd513461660468349f
Kosov234 <32747671+Kosov234@users.noreply.github.com>
2018-01-25 23:04:09 +0200

    AverageTests
    Still trying.

---
+++ TestsHomework(They doesn't really work ;(  )/AverageTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AverageTest
+    {
+        [TestMethod]
+    [ExpectedException(typeof(DivideByZeroException))]
+        public void TestAverageWithZero()
+        {
+            Methods.BasicMethods.Average(0);
+        }
+    }
+}
