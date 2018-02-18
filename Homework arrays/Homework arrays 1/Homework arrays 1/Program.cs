51c14f55c53f7c129ba638020ad61df28ee73431
Kosov234 <32747671+Kosov234@users.noreply.github.com>
2018-02-18 20:27:12 +0200

    Homework arrays
    Don't know how to make program read sentences by itself.

---
+++ Homework arrays/Homework arrays 1/Homework arrays 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_arrays_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = { "Wrong", "Is", "Sentence", "This" };
+            int length = array.Length;
+            string[] reversed = new string[length];
+            for (int index = 0; index < length; index++)
+            {
+                reversed[length - index - 1] = array[index];
+            }
+            for (int index = 0; index < length; index++)
+            {
+                Console.Write(reversed[index] + " ");
+            }
+
+        }
+
+
+
+    }
+}
