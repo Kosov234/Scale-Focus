218a9bfbf15ad2867c8c84be412c8f93c12af463
Kosov234 <32747671+Kosov234@users.noreply.github.com>
2018-03-25 17:20:13 +0300

    Objects and Classes Homework

---
+++ ObjectsAndClassesHOmewrok/ObjectsAndClassesHOmewrok/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsAndClassesHOmewrok
+{
+    class Car
+    {
+        public int ImmobilizerId { get; set }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public Array[] Tyre = new Tyre;
+        public IgnitionKey IgnitionKey { set; get; }
+        public Engine Engine { set; get; }
+        public Human Driver { set; get; }
+    }
+
+    public string EnterTheCar()
+    { if (Human.Age >= 18)
+        {
+            Console.WriteLine("You are allowed to drive!");
+        }
+        else 
+        {
+            Console.WriteLine("You are not allowed to drive!");
+        }
+    }
+}
