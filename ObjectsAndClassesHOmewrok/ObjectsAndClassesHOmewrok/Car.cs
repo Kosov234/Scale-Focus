0e729e1b4abab0e3daec99f7d27832c18f532414
Kosov234 <32747671+Kosov234@users.noreply.github.com>
2018-03-25 17:38:43 +0300

    Adding StartEngine method

--- ObjectsAndClassesHOmewrok/ObjectsAndClassesHOmewrok/Car.cs
+++ ObjectsAndClassesHOmewrok/ObjectsAndClassesHOmewrok/Car.cs
@@ -8,23 +8,42 @@ namespace ObjectsAndClassesHOmewrok
 {
     class Car
     {
-        public int ImmobilizerId { get; set }
+        public int ImmobilizerId { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
-        public Array[] Tyre = new Tyre;
+        public Array[] Tyre = new Array[4];
         public IgnitionKey IgnitionKey { set; get; }
         public Engine Engine { set; get; }
         public Human Driver { set; get; }
     }
 
     public string EnterTheCar()
-    { if (Human.Age >= 18)
+    {
+        if (Human.Age >= 18)
         {
             Console.WriteLine("You are allowed to drive!");
         }
-        else 
+        else
         {
             Console.WriteLine("You are not allowed to drive!");
         }
+    }
+
+    public string StartEngine()
+    {
+        if (Car.ImmobilizerId == IgnitionKey.ImmobilizerId)
+        {
+            if (Engine.IsRunning == false)
+            {
+                Console.WriteLine("Everything is OK");
+                Engine.IsRunning = true;
+            }
+            else
+            {
+                Console.WriteLine("Engine is already running!");
+            }
+        }
+        else
+        { Console.WriteLine("Ignition Key is not from this car"); }
     }
 }
